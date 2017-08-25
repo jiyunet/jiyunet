cdccb2a902055a0cf7daa4d80ba1806bd67a679a
treyzania <treyzania@gmail.com>
2017-08-24 21:11:01 -0400

    Moved towards a data architecture to abstract away signing logic.

--- dag/src/dag.rs
+++ dag/src/dag.rs
@@ -1,20 +1,110 @@
 use ident;
 
-pub type Address = [u8; 32];
+use crypto::sha2;
+use crypto::digest::Digest;
+
+pub type Address = ident::Hash;
 
 #[derive(Copy, Clone)]
 pub struct Signature {
-    signed_hash: [u8; 32],
-    signer: Address
+    hash_sig: ident::HashSig,
+    author: ident::Hash
+}
+
+impl Signature {
+    pub fn to_blob(&self) -> Vec<u8> {
+
+        let mut buf = Vec::new();
+        buf.extend_from_slice(&self.hash_sig);
+        buf.extend_from_slice(&self.author);
+        buf
+
+    }
+}
+
+pub struct DecodeError;
+
+pub trait DagComponent where Self: Clone {
+
+    fn from_blob(data: &[u8]) -> Result<Self, DecodeError>;
+    fn to_blob(&self) -> Vec<u8>;
+
+    fn get_hash(&self) -> ident::Hash {
+
+        let mut hasher = sha2::Sha256::new();
+        hasher.input(self.to_blob().as_slice());
+
+        let mut hashed: [u8; 32] = Default::default();
+        hasher.result(&mut hashed);
+        hashed
+
+    }
+
+    fn to_signed(self, kp: ident::Keypair) -> Signed<Self> {
+        Signed::new(kp, self)
+    }
+
+}
+
+#[derive(Clone)]
+pub struct Signed<T> where T: DagComponent {
+    sig: Signature,
+    body: T
+}
+
+impl<T> Signed<T> where T: DagComponent {
+
+    pub fn new(kp: ident::Keypair, body: T) -> Signed<T> {
+        Signed {
+            sig: Signature {
+                hash_sig: kp.sign(body.get_hash()),
+                author: kp.fingerprint()
+            },
+            body: body
+        }
+    }
+
+    pub fn extract(self) -> T {
+        self.body
+    }
+
+}
+
+impl<T> DagComponent for Signed<T> where T: DagComponent {
+
+    fn from_blob(data: &[u8]) -> Result<Self, DecodeError> {
+        unimplemented!();
+    }
+
+    fn to_blob(&self) -> Vec<u8> {
+
+        let mut buf = Vec::new();
+        buf.append(&mut self.sig.to_blob());
+        buf.append(&mut self.body.to_blob());
+        buf
+
+    }
+
 }
 
 #[derive(Clone)]
 pub struct Block {
-    sig: Signature,
     version: u32,
     timestamp: i64,
     parents: Vec<Address>,
-    segments: Vec<Segment>
+    segments: Vec<Signed<Segment>>
+}
+
+impl DagComponent for Block {
+
+    fn from_blob(_: &[u8]) -> Result<Self, DecodeError> {
+        unimplemented!();
+    }
+
+    fn to_blob(&self) -> Vec<u8> {
+        unimplemented!();
+    }
+
 }
 
 #[derive(Clone)]
@@ -26,9 +116,20 @@ pub enum SegmentContent {
 
 #[derive(Clone)]
 pub struct Segment {
-    sig: Signature,
     timestamp: i64,
     content: SegmentContent
+}
+
+impl DagComponent for Segment {
+
+    fn from_blob(_: &[u8]) -> Result<Self, DecodeError> {
+        unimplemented!();
+    }
+
+    fn to_blob(&self) -> Vec<u8> {
+        unimplemented!();
+    }
+
 }
 
 #[derive(Clone)]
@@ -37,10 +138,14 @@ pub struct ArtifactData {
     body: Vec<u8> // Actual artifact format is specified in a higher layer.
 }
 
-/// Used for storing artifacts off-chain, but still maintaining signature.
-#[derive(Clone)]
-pub struct Container {
-    sig: Signature,
-    timestamp: i64,
-    art: ArtifactData
+impl DagComponent for ArtifactData {
+
+    fn from_blob(_: &[u8]) -> Result<Self, DecodeError> {
+        unimplemented!();
+    }
+
+    fn to_blob(&self) -> Vec<u8> {
+        unimplemented!();
+    }
+
 }
--- dag/src/lib.rs
+++ dag/src/lib.rs
@@ -4,13 +4,3 @@ pub mod dag;
 pub mod ident;
 
 pub struct DecodeError;
-
-// A hash of all of the contents.
-pub trait DagComponent where Self: Sized, Self: Clone {
-
-    fn from_blob(data: &[u8]) -> Result<Self, DecodeError>;
-    fn into_blob(&self) -> &[u8];
-
-    fn get_address_hash(&self) -> dag::Address;
-
-}
