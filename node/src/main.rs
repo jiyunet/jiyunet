cdf144069d3645700522431a81c10f472907c3b5
treyzania <treyzania@gmail.com>
2017-11-03 14:07:57 -0400

    Set up node argument parsing.

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,20 @@
+extern crate libjiyunet_dag as dag;
+extern crate libjiyunet_db as db;
+extern crate libjiyunet_dht as dht;
+extern crate libjiyunet_validation as validation;
+
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+
+    let _matches = clap_app!(myapp =>
+            (version: "0.1")
+            (author: "The Jiyunet Developers")
+            (about: "// TODO Fill this in.")
+            (@arg BIND_PORT: -p --bind-port +takes_value "Specifies the port for the daemon to bind to.  Default: 8200")
+            (@arg DATA_DIR: -d --data-dir +takes_value "Specifies the block/validation data directory.  Default: /var/lib/jiyunet")
+            (@arg CACHE_DIR: -c --cache-dir +takes_value "Specified the artifact cache directory.  Default: /var/cache/jiyunet/artifact")
+        ).get_matches();
+
 }
