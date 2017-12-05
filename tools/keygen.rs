667c273c8b9e65ddf972794ab5939f27a794e6f8
treyzania <treyzania@gmail.com>
2017-12-05 16:54:51 -0500

    Made the keygen program more cryptographically secure.

--- tools/keygen.rs
+++ tools/keygen.rs
@@ -1,9 +1,13 @@
 extern crate jiyunet_core as core;
 
+extern crate rand;
+
 use std::env;
 use std::fs;
 use std::path;
-use std::io::{self, Read, Write};
+
+use rand::Rng;
+use rand::os::OsRng;
 
 use core::io::BinaryComponent;
 use core::sig;
@@ -14,16 +18,36 @@ fn main() {
     let mut df = fs::File::create(path::PathBuf::from(dest.clone()).as_path())
                             .expect("unable to create destination file.");
 
-    let mut entropy = [0; 1024];
-    match io::stdin().read(&mut entropy) {
-        Ok(c) => println!("read {} bytes", c),
-        Err(e) => println!("error reading input: {}", e)
-    }
+    let mut seed = [0; 4096];
+    let mut rng = match OsRng::new() {
+        Ok(r) => r,
+        Err(e) => panic!("could not initialize RNG: {}", e)
+    };
 
-    let kp = sig::Scheme::Ed25519.generate(&entropy);
+    rng.fill_bytes(&mut seed);
+
+    // Actually generate the seed.
+    let kp = sig::Scheme::Ed25519.generate(&seed);
     match kp.to_writer(&mut df) {
         Ok(_) => println!("keypair saved to {}", dest),
         Err(_) => println!("unable to write to destination file")
     }
 
+    match kp {
+        sig::Keypair::Ed25519(k, p) => {
+            println!("scheme: ed25519");
+            println!("private key: {}", u8_slice_to_string(&k));
+            println!("public key: {}", u8_slice_to_string(&p));
+        }
+    }
+
+}
+
+fn u8_slice_to_string(data: &[u8]) -> String {
+    use std::fmt::Write;
+    let mut s = String::with_capacity(data.len() * 2);
+    for i in 0..data.len() {
+        write!(&mut s, "{:X}", data[i]).expect("aw shit");
+    }
+    s
 }
